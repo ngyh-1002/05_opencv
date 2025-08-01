a4f45e32edd98a20fd68351d87671f71ad102520
ngyh-1002 <01049200297aa@gmail.com>
2025-08-01 10:39:53 +0900

    스캔 이미지 디코딩
    스캔 이미지 디코딩

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -3,9 +3,14 @@ import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
 
 img = cv2.imread('../img/frame.png')
+# 스캔 이미지 그레이 스케일
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap='gray')
 plt.show()
+
+# 디코딩
+decoded = pyzbar.decode(gray)
+print(decoded)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
