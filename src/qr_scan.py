1d83d4dd564e298b6d87af38ce8cdc2351b2edca
ngyh-1002 <01049200297aa@gmail.com>
2025-08-01 10:37:58 +0900

    스캔이미지 그레이스케일
    스캔이미지 그레이스케일

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
 
 img = cv2.imread('../img/frame.png')
-plt.imshow(img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray, cmap='gray')
 plt.show()
 
 cv2.waitKey(0)
