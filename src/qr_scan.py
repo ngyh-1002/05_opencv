788f6bb30889d3cad5980cee80772e9c955c710a
ngyh-1002 <01049200297aa@gmail.com>
2025-08-01 11:22:52 +0900

    영상으로 이미지 연결
    영상으로 이미지 연결

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -2,29 +2,39 @@ import cv2
 import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
 
-img = cv2.imread('../img/frame.png')
-# 스캔 이미지 그레이 스케일
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 # plt.imshow(gray, cmap='gray')
 # plt.show()
 
-# 디코딩
-decoded = pyzbar.decode(gray)
-print(decoded)
+cap = cv2.VideoCapture(0)
 
-for d in decoded:
-    print(d.data.decode('utf-8'))
-    barcode_data = d.data.decode('utf-8')
-    print(d.type)
-    barcode_type = d.type
+while (cap.isOpened()):
+    ret, img = cap.read()
+    if not ret:
+        continue
+    img = cv2.imread('../img/frame.png')
+    # 스캔 이미지 그레이 스케일
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    text = '%s (%s)' % (barcode_data, barcode_type)
+    # 디코딩
+    decoded = pyzbar.decode(gray)
+    print(decoded)
 
+    for d in decoded:
+        print(d.data.decode('utf-8'))
+        barcode_data = d.data.decode('utf-8')
+        print(d.type)
+        barcode_type = d.type
 
-cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]), (0, 255, 0), 20)
-cv2.putText(img, text, (d.rect[0], d.rect[1]-50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (0, 255, 255), 2, cv2.LINE_AA)
-plt.imshow(img)
-plt.show()
-cv2.waitKey(0)
+        text = '%s (%s)' % (barcode_data, barcode_type)
+
+        cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]), (0, 255, 0), 20)
+        cv2.putText(img, text, (d.rect[0], d.rect[1]-50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (0, 255, 255), 2, cv2.LINE_AA)
+    cv2.imshow('camera', img)
+    
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+cap.release()
 cv2.destroyAllWindows()
+# plt.imshow(img)
+# plt.show()
