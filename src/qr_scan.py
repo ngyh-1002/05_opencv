3a36daeec823d740ddf1d8b3ddf192ea7a4c9b14
ngyh-1002 <01049200297aa@gmail.com>
2025-08-01 10:56:22 +0900

    스캔 이미지 부분에 사각형 그리기
    스캔 이미지 부분에 사각형 그리기

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -5,12 +5,21 @@ import pyzbar.pyzbar as pyzbar
 img = cv2.imread('../img/frame.png')
 # 스캔 이미지 그레이 스케일
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(gray, cmap='gray')
-plt.show()
+
+# plt.imshow(gray, cmap='gray')
+# plt.show()
 
 # 디코딩
 decoded = pyzbar.decode(gray)
 print(decoded)
 
+for d in decoded:
+    print(d.data.decode('utf-8'))
+    print(d.type)
+
+cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]), (0, 255, 0), 2)
+
+plt.imshow(img)
+plt.show()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
